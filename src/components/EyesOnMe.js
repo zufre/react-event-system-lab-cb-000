cb0acfa45356284e118ee0ca9f92029dc39e7b8d
Cedric David <cedric.david@btopenworld.com>
2018-10-10 06:10:42 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import React from 'react';
 
@@ -8,6 +9,15 @@ export default class EyesOnMe extends React.Component {
   render() {
     return (
       <button onFocus={this.focus} onBlur={this.blur}>Button</button>
+=======
+// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  render() {
+    return (
+      <button onFocus={console.log('Good!') onBlur={console.log('Hey! Eyes on me!')}></button>
+>>>>>>> 71c8db02250e63547d7ae1d73a7bfa242cf5965d
     )
   }
 }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,12 +2,18 @@
 import React from 'react'
 
 export default class Keypad extends React.Component {
+<<<<<<< HEAD
   entering = () => {
     console.log('Entering password...')
   }
   render() {
     return (
       <input onKeyUp={this.entering} type="password" />
+=======
+  render() {
+    return (
+      <input type="password" onKeyUp={console.log('Entering password...')}/>
+>>>>>>> 71c8db02250e63547d7ae1d73a7bfa242cf5965d
     )
   }
 }
